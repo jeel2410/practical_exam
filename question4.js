cbf199c8c1b39b104ad1e28e467ca009ffdbb81e
jeelTecheniac <jeel@techeniac.com>
2025-07-22 17:16:52 +0530

    fix: updated res

--- question4.js
+++ question4.js
@@ -1,25 +1,22 @@
 function addChar(str) {
-    let len = str.length;
-    let s = 0;
-    let e = len - 1;
-    let res = 0;
+  let len = str.length;
+  let s = 0;
+  let e = len - 1;
+  let res = 1;
 
-    while (s < e) {
-        if (str[s] === str[e]) {
-            s++;
-            e--;
-        }
-        else {
-            res++;
-            s = 0;
-            e = len - res - 1;
-        }
+  while (s < e) {
+    if (str[s] === str[e]) {
+      s++;
+      e--;
+    } else {
+      res++;
+      s = 0;
+      e = len - res - 1;
     }
-    return res;
-
-
+  }
+  return res;
 }
 
-console.log(addChar("abc"))
+console.log(addChar("abc"));
 console.log(addChar("aacecaaa"));
-console.log(addChar("madam")); 
+console.log(addChar("madam"));
